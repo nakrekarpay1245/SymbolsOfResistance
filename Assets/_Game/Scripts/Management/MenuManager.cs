c20eae6f083bb9e6c571c951978ddb158d84dffa
nakrekarpay1245 <rknyprk2001@gmail.com>
2024-07-20 16:06:58 +0300

    feat: Credits Menu updated

--- Assets/_Game/Scripts/Management/MenuManager.cs
+++ Assets/_Game/Scripts/Management/MenuManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using TMPro;
 using System.Collections.Generic;
+using DG.Tweening;
 
 /// <summary>
 /// Manages the menu interactions and scene transitions.
@@ -12,22 +14,48 @@ public class MenuManager : MonoBehaviour
     #region PARAMS
     [Header("MenuManager Params")]
     [Tooltip("List of LeafButton components in the menu.")]
-    [SerializeField] private List<LeafButton> leafButtons;
+    [SerializeField] private List<LeafButton> _leafButtons;
+
+    [Header("Music Params")]
+    [Tooltip("List of background music clips.")]
+    [SerializeField] private List<AudioClip> _musicClips;
+    //TextMeshPro component for the music button
+    private TextMeshProUGUI _musicButtonText;
+    [Tooltip("LeafButton component for the music button.")]
+    [SerializeField] private LeafButton _musicButton;
+    [Tooltip("AudioSource component for the background music.")]
+    [SerializeField] private AudioSource _audioSource;
     #endregion
 
     private int _currentButtonIndex = 0;
+    private int _currentMusicIndex = 0;
 
     private void Awake()
     {
         // Ensure there are LeafButtons in the list
-        if (leafButtons == null || leafButtons.Count == 0)
+        if (_leafButtons == null || _leafButtons.Count == 0)
         {
             Debug.LogError("No LeafButtons assigned in the MenuManager.");
             return;
         }
 
+        // Ensure there are music clips
+        if (_musicClips == null || _musicClips.Count == 0)
+        {
+            Debug.LogError("No music clips assigned in the MenuManager.");
+            return;
+        }
+
+        // Get the AudioSource component
+        _audioSource = GameObject.Find("BackgroundMusic").GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            Debug.LogError("No AudioSource found on BackgroundMusic object.");
+            return;
+        }
+
         // Assign functions to buttons
-        foreach (var button in leafButtons)
+        foreach (var button in _leafButtons)
         {
             if (button.name == "StartButton")
             {
@@ -38,12 +66,25 @@ public class MenuManager : MonoBehaviour
                 button.OnPressed.AddListener(Quit);
             }
         }
+
+        // Assign function to the music button
+        if (_musicButton != null)
+        {
+            _musicButtonText = _musicButton.GetComponent<TextMeshProUGUI>();
+            _musicButton.OnPressed.AddListener(ChangeMusic);
+        }
+        else
+        {
+            Debug.LogError("MusicButton not assigned in the MenuManager.");
+        }
     }
 
     private void Start()
     {
         // Highlight the first button initially
         HoverButton(_currentButtonIndex);
+        // Start the first music track
+        PlayMusic(_currentMusicIndex);
     }
 
     private void Update()
@@ -75,7 +116,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     private void MoveToNextButton()
     {
-        _currentButtonIndex = (_currentButtonIndex + 1) % leafButtons.Count;
+        _currentButtonIndex = (_currentButtonIndex + 1) % _leafButtons.Count;
         HoverButton(_currentButtonIndex);
     }
 
@@ -84,7 +125,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     private void MoveToPreviousButton()
     {
-        _currentButtonIndex = (_currentButtonIndex - 1 + leafButtons.Count) % leafButtons.Count;
+        _currentButtonIndex = (_currentButtonIndex - 1 + _leafButtons.Count) % _leafButtons.Count;
         HoverButton(_currentButtonIndex);
     }
 
@@ -93,7 +134,7 @@ public class MenuManager : MonoBehaviour
     /// </summary>
     private void PressCurrentButton()
     {
-        var currentButton = leafButtons[_currentButtonIndex];
+        var currentButton = _leafButtons[_currentButtonIndex];
         ExecuteEvents.Execute(currentButton.gameObject, new PointerEventData(EventSystem.current),
             ExecuteEvents.pointerDownHandler);
     }
@@ -104,18 +145,18 @@ public class MenuManager : MonoBehaviour
     /// <param name="index">Index of the button to highlight.</param>
     private void HoverButton(int index)
     {
-        for (int i = 0; i < leafButtons.Count; i++)
+        for (int i = 0; i < _leafButtons.Count; i++)
         {
             if (i == index)
             {
-                ExecuteEvents.Execute(leafButtons[i].gameObject, new PointerEventData(EventSystem.current),
+                ExecuteEvents.Execute(_leafButtons[i].gameObject, new PointerEventData(EventSystem.current),
                     ExecuteEvents.pointerEnterHandler);
 
-                Debug.Log(leafButtons[_currentButtonIndex].name + " Hover");
+                //Debug.Log(leafButtons[_currentButtonIndex].name + " Hover");
             }
             else
             {
-                ExecuteEvents.Execute(leafButtons[i].gameObject, new PointerEventData(EventSystem.current),
+                ExecuteEvents.Execute(_leafButtons[i].gameObject, new PointerEventData(EventSystem.current),
                     ExecuteEvents.pointerExitHandler);
             }
         }
@@ -136,6 +177,31 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
     #endregion
+
+    /// <summary>
+    /// Changes to the next music track and updates the button text.
+    /// </summary>
+    private void ChangeMusic()
+    {
+        _currentMusicIndex = (_currentMusicIndex + 1) % _musicClips.Count;
+        PlayMusic(_currentMusicIndex);
+    }
+
+    /// <summary>
+    /// Plays the specified music track with fade-in and pitch effects.
+    /// </summary>
+    /// <param name="index">Index of the music track to play.</param>
+    private void PlayMusic(int index)
+    {
+        _audioSource.DOFade(0, 0.5f).OnComplete(() =>
+        {
+            _audioSource.clip = _musicClips[index];
+            _audioSource.Play();
+            _audioSource.DOFade(1, 0.5f);
+            _musicButtonText.text = _audioSource.clip.name;
+        });
+        _audioSource.DOPitch(1.5f, 0.5f).SetLoops(2, LoopType.Yoyo);
+    }
 
     public void OpenUrl(string URL)
     {
